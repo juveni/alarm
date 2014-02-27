d0f751f1050ba51313b3b963e0bf961c1c518a8f
Miguel <miguelavaltroni@gmail.com>
2014-02-27 10:03:34 -0300

    update31

--- js/main.js
+++ js/main.js
@@ -11,29 +11,17 @@ var init = function () {
     
 
     notificationMe = function () {
-    	//try {
-    		if(tizen)
-    			alert("Tem Tizen");
-    		if(tizen.alarm)
-    			alert("Tem tizen alarm");
-    		if(tizen.ApplicationControl)
-    			alert("Tem tizen application Control");
-    		if(window.tizen)
-    			alert("Tem window Tizen");
-    		if(window.tizen.alarm)
-    			alert("Tem window tizen alarm");
-    		if(window.tizen.ApplicationControl)
-    			alert("Tem window tizen application Control");
-    		//window.tizen.alarm.removeAll();
-    		//var alarm = new tizen.AlarmRelative(5);
+    	try {
+    		console.log(tizen.application);
+    		window.tizen.alarm.removeAll();
     		var appControl = new window.tizen.ApplicationControl("http://tizen.org/appcontrol/operation/view");
-    		var alarm = new window.tizen.AlarmAbsolute(new Date(2014, 1, 27, 10, 00));
+    		var alarm = new window.tizen.AlarmAbsolute(new Date(2014, 1, 27, 10, 20));
     		window.tizen.alarm.add(alarm, tizen.application.getCurrentApplication().appInfo.id, appControl);
     		alert('Prepare to Tizen Alert');
     		alert(alarm.getNextScheduledDate()+" - "+window.tizen.alarm.getAll().length);
-    	//} catch(err) {
-    	//	alert(err.name+" : "+err.message);
-    	//}
+    	} catch(err) {
+    		alert(err.name+" : "+err.message);
+    	}
     }
 
 };
